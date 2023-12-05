4bf46af52390c3903370e19cf294528fd5cfdaf4
colecandoe <coledouglasmiller@gmail.com>
2023-12-04 16:17:17 -0800

    init

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INPUT(var) for(string var; getline(cin, var), var!="";)
+#define FOR(i, s, e) for (int i=s; i<e; ++i)
+
+void part1() {
+
+}
+
+void part2() {
+
+}
+
+void main() {
+	part1();
+	//part2();
+}
